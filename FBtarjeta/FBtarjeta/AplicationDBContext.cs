bf356420919a2450f3c83c27668f60f52584a65d
Joaquin Cavenaghi <joaquincavenaghi@gmail.com>
2024-06-19 10:46:45 -0300

    Feat: simple proyecto de listado y administración de tarjettas.

---
+++ FBtarjeta/FBtarjeta/AplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using FBtarjeta.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FBtarjeta
+{
+    public class AplicationDBContext: DbContext
+    {
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options): base(options) 
+        {
+
+        }
+    }
+}
